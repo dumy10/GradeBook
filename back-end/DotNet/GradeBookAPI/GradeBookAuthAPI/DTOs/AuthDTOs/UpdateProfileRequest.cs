02afb99e951af538961afc0fe210632960547634
Valy2255 <ghitavalentin70@yahoo.com>
2025-03-20 15:25:50 +0200

    SCRUM-7: implement user profile management

---
+++ back-end/DotNet/GradeBookAPI/GradeBookAuthAPI/DTOs/AuthDTOs/UpdateProfileRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GradeBookAuthAPI.DTOs.AuthDTOs
+{
+    public class UpdateProfileRequest
+    {
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(20)]
+        public string Phone { get; set; }
+
+        [StringLength(255)]
+        public string Address { get; set; }
+    }
+}
